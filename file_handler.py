198d2514d35a87493d73f646a16a06467323de2c
laura z shaw <lzshaw@lauras-Air.home>
2016-08-23 20:30:55 -0400

    zachs refactor

--- file_handler.py
+++ file_handler.py
@@ -25,36 +25,87 @@ def _check_dictionary(plot_obj):
         return 'ok'
     except KeyError:
         return 'error: missing key'
+
+def is_long(num):
+    '''workaround so python2 tests ints and longs and python3 tests ints'''
+    try:
+        return isinstance(num, (int, long))
+    except NameError:
+        return isinstance(num, int)
+
 def _check_values(plot_obj):
     '''checks all the values are the right kinds.  returns 'error:<details>'
     or 'ok'.'''
     msg = 'error:'
-    x_min, x_max = plot_obj['x_range']
-    y_min, y_max = plot_obj['y_range']
-    def is_long(num):
-        '''workaround so python2 tests ints and longs and python3 tests ints'''
-        try:
-            return isinstance(num, (int, long))
-        except NameError:
-            return isinstance(num, int)
+    msg += check_x_range_are_longs_maybe_return_error_msg(plot_obj)
+    msg += check_y_range_are_floats_maybe_return_error_msg(plot_obj)
+    msg += check_tuple_list_feq_and_val_are_longs_maybe_return_error_msg(plot_obj)
+    msg += check_pts(plot_obj)
+    msg += check_dice_list(plot_obj)
+    if msg == 'error:':
+        msg = 'ok'
+    return msg
 
-    if not is_long(x_min) or not is_long(x_max):
-        msg += ' incorrect x_range'
-    if not isinstance(y_min, float) or not isinstance(y_max, float):
-        msg += ' incorrect y_range'
-    for freq, val in plot_obj['tuple_list']:
-        if not is_long(freq) or not is_long(val):
-            msg += ' corrupted "tuple_list"'
+def flatten_tuple_list(tuple_list):
+    output = []
+    for a_tuple in tuple_list:
+        output.extend(list(a_tuple))
+    return output
+
+def check_tuple_list_feq_and_val_are_longs_maybe_return_error_msg(plot_obj):
+    msg = ''
+    tuples_list_as_list = flatten_tuple_list(plot_obj['tuple_list'])
+    if not check_all_numbers_are_longs(tuples_list_as_list):
+        msg += ' corrupted "tuple_list"'
+    return msg
+
+def check_pts(plot_obj):
+    plot_pts = flatten_tuple_list(plot_obj['pts'])
+    if not check_all_numbers_are_floats_or_ints(plot_pts):
+        return ' corrupted "pts"'
+    return ''
+
+def check_pts_old(plot_obj):
+    msg = ''
     for tuple_ in plot_obj['pts']:
         for val in tuple_:
             if not isinstance(val, (int, float)):
                 msg += ' corrupted "pts"'
+    return msg
+
+def check_dice_list(plot_obj):
+    msg = ''
     for die, num in plot_obj['dice']:
         if not isinstance(die, dt.ProtoDie) or not isinstance(num, int):
             msg += ' dicelist at ({!r}, {})'.format(die, num)
-    if msg == 'error:':
-        msg = 'ok'
     return msg
+
+def check_x_range_are_longs_maybe_return_error_msg(plot_obj):
+    x_range = plot_obj['x_range']
+    if not check_all_numbers_are_longs(x_range):
+        return ' incorrect x_range'
+    return ''
+
+def check_y_range_are_floats_maybe_return_error_msg(plot_obj):
+    y_range = plot_obj['y_range']
+    if not check_all_numbers_are_floats(y_range)
+        return ' incorrect y_range'
+    return ''
+
+def check_all_numbers_are_longs(numbers):
+    all_longs = True
+    for number in numbers:
+        all_longs = all_longs and is_long(number)
+    return all_longs
+ 
+def check_all_numbers_are_floats(numbers):
+    all_floats = True
+    for number in numbers:
+        all_floats = all_floats and isinstance(number, float) 
+    return all_floats
+
+########=====###
+
 def check_data(plot_obj):
     '''checks history to see if plot_obj has expected data.  if ok, returns 'ok'
     else returns a msg starting with 'error:' '''
